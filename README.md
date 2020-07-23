a8c2e270ea20561fb362a628014b520b29ae0678
NFSS10 <nuno_nfss96@hotmail.com>
2020-07-23 21:27:52 +0100

    Added server

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ESP32-CAM
 
 Scripts for Arduino IDE
+
+
+## How to run the server:
+`node app`
---
+++ server/app.js
@@ -0,0 +1,18 @@
+const fastify = require('fastify')({ logger: true })
+
+// Routes
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+})
+
+// Start server
+const start = async () => {
+    try {
+        await fastify.listen(3000, "0.0.0.0")
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
