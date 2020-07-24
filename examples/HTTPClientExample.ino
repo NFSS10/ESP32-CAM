eabf830c8e31926b94ed84346c11924c933437b4
NFSS10 <nuno_nfss96@hotmail.com>
2020-07-24 21:48:33 +0100

    Update HTTPClientExample.ino

--- examples/HTTPClientExample.ino
+++ examples/HTTPClientExample.ino
@@ -1,34 +1,25 @@
+#include "wifi.h"
 #include "http_client.h"
-
-const char *ssid = "Rede";
-const char *password = "rede_password";
 
 void setup()
 {
-	Serial.begin(115200);
+  Serial.begin(115200);
 
-	WiFi.begin(ssid, password);
-	Serial.print("Connecting to WiFi...");
-
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		delay(500);
-		Serial.print(".");
-	}
-
-	Serial.println("\nConnected to WiFi !!!\n");
-	Serial.print("IP address: ");
-	Serial.print(WiFi.localIP());
+  wifi_connect("Rede", "rede_password");
 }
 
 void loop()
 {
-	if ((WiFi.status() == WL_CONNECTED))
-	{
-		response res = http_get("http://192.168.1.68:3000/");
-		Serial.println(res.status_code);
-		Serial.println(res.body);
-	}
+  if ((WiFi.status() == WL_CONNECTED))
+  {
+    Serial.println("\nTesting GET request ...");
+    response res = http_get("http://192.168.1.68:3000/");
+    
+    Serial.print("Status code: ");
+    Serial.println(res.status_code);
+    Serial.println("Response:");
+    Serial.println(res.body);
+  }
 
-	delay(3000);
+  delay(3000);
 }
